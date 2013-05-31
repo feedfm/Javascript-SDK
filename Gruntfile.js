7a003c84f7b3c0c048b5809ccb22a2cf90c111c7
Eric Lambrecht <eml@ill.com>
2013-05-31 12:13:40 -0700

    add minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Project configuration.
   grunt.initConfig({
@@ -26,12 +27,42 @@ module.exports = function(grunt) {
       all: [
         'src/*.js'
       ]
-    }
+    },
 
+    uglify: {
+      options: {
+        compress: true,
+        report: 'gzip',
+        preserveComments: 'some',
+        banner: '/* A Feed.fm joint: github.com/fuzz-radio/Javascript-SDK */'
+      },
+
+      dist: {
+        files: {
+          'dist/feed.js': [ 
+            'lib/json2.js',
+            'lib/hmac-sha256.js',
+            'lib/enc-base64.js',
+            'lib/oauth.js',
+            'lib/jquery.js',
+            'lib/jquery.cookie.js',
+            'lib/underscore.js',
+            'lib/soundmanager2.js',
+
+            'src/events.js',
+            'src/log.js',
+            'src/speaker.js',
+            'src/session.js',
+            'src/player.js',
+            'src/player-view.js'
+          ]
+        }
+      }
+    }
   });
 
   // Default task.
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['jshint', 'uglify:dist']);
 
 };
 
