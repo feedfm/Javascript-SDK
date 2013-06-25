30889e9924be6e656b6a691198f1b85be5713791
Eric Lambrecht <eml@ill.com>
2013-06-24 20:28:08 -0700

    add concat option

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Project configuration.
   grunt.initConfig({
@@ -27,6 +28,82 @@ module.exports = function(grunt) {
       all: [
         'src/*.js'
       ]
+    },
+
+    concat: {
+      dist: {
+        files: {
+          'dist/feed-with-jquery.js': [ 
+            'lib/json2.js',
+            'lib/hmac-sha256.js',
+            'lib/enc-base64.js',
+            'lib/oauth.js',
+            'lib/jquery.js',
+            'lib/jquery.cookie.js',
+            'lib/underscore.js',
+            'lib/soundmanager2.js',
+
+            'src/events.js',
+            'src/nolog.js',
+            'src/speaker.js',
+            'src/session.js',
+            'src/player.js',
+            'src/player-view.js'
+          ],
+
+          'dist/feed-with-jquery-debug.js': [ 
+            'lib/json2.js',
+            'lib/hmac-sha256.js',
+            'lib/enc-base64.js',
+            'lib/oauth.js',
+            'lib/jquery.js',
+            'lib/jquery.cookie.js',
+            'lib/underscore.js',
+            'lib/soundmanager2.js',
+
+            'src/events.js',
+            'src/log.js',
+            'src/speaker.js',
+            'src/session.js',
+            'src/player.js',
+            'src/player-view.js'
+          ],
+
+          'dist/feed-without-jquery.js': [
+            'lib/json2.js',
+            'lib/hmac-sha256.js',
+            'lib/enc-base64.js',
+            'lib/oauth.js',
+            'lib/jquery.cookie.js',
+            'lib/underscore.js',
+            'lib/soundmanager2.js',
+
+            'src/events.js',
+            'src/nolog.js',
+            'src/speaker.js',
+            'src/session.js',
+            'src/player.js',
+            'src/player-view.js'
+          ],
+
+          'dist/feed-without-jquery-debug.js': [ 
+            'lib/json2.js',
+            'lib/hmac-sha256.js',
+            'lib/enc-base64.js',
+            'lib/oauth.js',
+            'lib/jquery.cookie.js',
+            'lib/underscore.js',
+            'lib/soundmanager2.js',
+
+            'src/events.js',
+            'src/log.js',
+            'src/speaker.js',
+            'src/session.js',
+            'src/player.js',
+            'src/player-view.js'
+          ]
+        }
+      }
     },
 
     uglify: {
