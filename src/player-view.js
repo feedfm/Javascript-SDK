3a60fc62a3f5de22e176e7cbfbc7d4476e7a56aa
Eric Lambrecht <eml@ill.com>
2014-10-09 12:06:27 -0700

    referring to wrong variable

--- src/player-view.js
+++ src/player-view.js
@@ -142,7 +142,7 @@ define([ 'underscore', 'jquery' ], function(_, $) {
   };
 
   PlayerView.prototype._onPlacement = function(placement) {
-    if (!this._originalDisplayText) {
+    if (!this.originalDisplayText) {
       this.originalDisplayText = this.formatPlacement(placement);
 
       this.renderStatus(this.originalDisplayText);
